244d4036b13005768c0cdd0ab8806d223f34f49c
Bill Liza <roary@fiu.edu>
2022-01-26 23:28:29 -0500

    auth working. SignUp not showing when click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import './App.css';
-
 import { Container } from 'react-bootstrap'
+
 import {
   BrowserRouter as Router,
   Route,
   Navigate,
   Routes
 } from 'react-router-dom'
+
 import Home from "./Pages/Home/Home";
 import Explore from "./Pages/Explore/Explore";
 import Premium from "./Pages/Premium/Premium";
 import NavBar from "./Components/Navbar/Navbar";
-import SignUp from "./Components/SignUp/signup";
+import SignUp from "./Pages/SignUp/signup";
 import { AuthProvider } from "./contexts/AuthContexts";
  
 const App = () => {
   return (
-
-    <AuthProvider>
       <Container>
           <Router>
             <NavBar />
@@ -27,13 +26,16 @@ const App = () => {
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/explore" element={<Explore />} />
                 <Route exact path="/premium" element={<Premium />} />
-                <Route exact path="/signon" element={<SignUp />} />
+                <Route exact path="/signup" element={
+                  <AuthProvider>
+                    <SignUp/> 
+                  </AuthProvider>
+                } />
                 <Route path="*" element={<Navigate to="/" />} />
               </Routes>
             </main>
           </Router>
       </Container>
-    </AuthProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app'
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,15 +10,25 @@ import 'firebase/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
+/*const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+})*/
+
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_CODE2EARN_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_CODE2EARN_FIREBASE_AUTH_DOMAIN ,
-    projectId: process.env.REACT_CODE2EARN_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_CODE2EARN_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_CODE2EARN_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_CODE2EARN_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_CODE2EARN_FIREBASE_MEASUREMENT_ID
-})
+    apiKey: "AIzaSyAfCx0wGNS6EvO3E2w8J9-f9T88M18NCl0",
+    authDomain: "genial-beaker-339403.firebaseapp.com",
+    projectId: "genial-beaker-339403",
+    storageBucket: "genial-beaker-339403.appspot.com",
+    messagingSenderId: "728702184518",
+    appId: "1:728702184518:web:5327d19faa138347ddee47",
+    measurementId: "G-0703XKHMD2"
+  });
 
 export const auth = app.auth()
 export default app
